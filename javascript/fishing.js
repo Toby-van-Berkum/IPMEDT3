4054a62883d2aa9d661dd3646c517e6c3828a617
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2024-01-22 14:25:44 +0100

    lmao

--- javascript/fishing.js
+++ javascript/fishing.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function loop() {
         var hitboxFrontColor = hitboxFront.getAttribute("color");
+        
         if (hitboxFrontColor == "green") {
             console.log("good!");
             dobber.setAttribute("visible", "true");
             dobber.setAttribute("position", "1 1 1");
-            
+
         }
 
         setTimeout(loop, 10);
