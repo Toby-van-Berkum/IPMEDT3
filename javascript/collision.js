f13aaad6a9fda974533dff1651295f7ca03896f9
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2024-01-24 14:51:31 +0100

    added third hitbox (for retrieving fish)

--- javascript/collision.js
+++ javascript/collision.js
@@ -7,6 +7,8 @@ AFRAME.registerComponent('collision-check', {
     this.tickCounter = 0;
     //firstHit is used to register wether the rear hitbox has been hit or not
     this.firstHit = false;
+    this.goodThrow = false;
+    this.goodCatch = true;
 
   },
   tick: function () {
@@ -20,22 +22,35 @@ AFRAME.registerComponent('collision-check', {
           console.log('Element is in rear hitbox');
           this.tickCounter = 0;
           this.firstHit = true;
-          document.getElementById('howTo').setAttribute('value', 'Zwaai nu je hengel in een boog over \nje hooft naar voren om in te gooien!')
+          document.getElementById('howTo').setAttribute('value', 'Zwaai nu je hengel in een boog over \nje hooft naar voren om in te gooien!');
 
         }
         //checks for collision with the front hitbox
         if (intersectedEl.id === 'hitbox-front' && this.tickCounter < 300 && this.firstHit == true) {
           console.log("good throw");
-          document.getElementById('howTo').setAttribute('value', 'Goed gedaan!\nNu is het wachten tot dat je beet hebt.\nAls je ziet dat de dobber het water in wordt getrokken trek je de hengel om hoog om \nde vis in te haken!')
+          document.getElementById('howTo').setAttribute('value', 'Goed gedaan!\nNu is het wachten tot dat je beet hebt.\nAls je ziet dat de dobber het water in wordt getrokken trek je de hengel om hoog om \nde vis in te haken!');
           this.firstHit = false;
+          this.goodThrow = true;
+        }
+
+        //checks for collision with mid hitbox
+        if (intersectedEl.id === 'hitbox-mid' && this.goodThrow == true && this.goodCatch == true) {
+          document.getElementById('howTo').setAttribute('value', 'Goed gedaan! Je hebt een vis gevangen!');
+          console.log('ur mom');
         }
       });
     }
 
     //counts the amount of ticks that have past and resets after 10 seconds
     this.tickCounter++;
-    if (this.tickCounter >1000) {
+    if (this.tickCounter >300) {
       this.tickCounter = 0;
+      this.firstHit = false;
+
+      //resets text message in case of bad throw
+      if (this.goodThrow == false) {
+        document.getElementById('howTo').setAttribute('value', 'Hi welkom bij onze fishing tutorial! \nOm te beginnen breng je vishengel over\n je schouder naar achter.');
+      }
     }
   },
   handleIntersect: function (event) {
@@ -44,4 +59,7 @@ AFRAME.registerComponent('collision-check', {
   handleIntersectCleared: function (event) {
     // This function can be empty, or you can add specific handling when intersected is cleared
   }
+  
 });
+
+
